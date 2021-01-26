af16af7ce88d647c74426a018fd74fb3f482d889
Zane Showalter-Castorena <Zane.Showalter.Castorena@gmail.com>
2021-01-25 20:57:52 -0700

    First attempt at hw1

--- hw1/first_test.m
+++ hw1/first_test.m
@@ -3,8 +3,9 @@ function z=first_test(x,y)
 % This is just to set up and test the homework workflow.
 % Add a line of code below that will compute matrix z as matrix x times matrix y.
 
-
+    z = x*y;
 % To get the credit, this function needs to work and return the correct value.
 % Do not change the name of the file or the function line in any way, it would not run.
 % You may want to write another function to test this function.
 end
+
