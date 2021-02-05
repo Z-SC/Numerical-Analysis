c2843459d2e24e5395a123763c03d9d9a2a7e046
Jan Mandel <jan.mandel@gmail.com>
2021-02-04 22:25:44 -0700

    adding file find_next.m

---
+++ hw3/find_next.m
@@ -0,0 +1,12 @@
+function y=find_next(x)
+% b=find_next(a)
+% find the smallest number y such that y>x
+% in:
+%   x   size (1,1), double
+% out:
+%   y  size(1,1), double
+
+% your code here
+
+
+end
